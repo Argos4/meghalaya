78ebf9f95a90879358410fd0228bff9dae72ad27
Aniket Harkare <aniket.harkare@gmail.com>
2023-05-24 14:33:49 -0400

    Adding base project

---
+++ main.py
@@ -0,0 +1,41 @@
+from typing import Union
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+import validation
+
+class AppProfile(BaseModel):
+    icto_id : str
+
+class CommonTags(BaseModel):
+    business_unit: str
+    environment: str
+    icto: str
+
+class CosmosDBAccount(BaseModel):
+    azurerm_cosmosdb_account_name: str
+    azurerm_cosmosdb_account_offertype: str
+    azurerm_cosmosdb_account_kind: str
+
+class DataModel(BaseModel):
+    app_profile: AppProfile
+    common_tags: CommonTags
+    capabilities_list: List[str]
+    resource_group_name: str
+    resource_group_location: str
+    azurerm_cosmosdb_account: CosmosDBAccount
+
+
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    return {"Welcome to Meghalaya !!"}
+
+@app.post("/build_resource/")
+async def build_resource(resource: DataModel):
+    if validation.validate_icto(resource.app_profile.icto_id) == True :
+        return {"Input for Cosmos DB": "valid Response"}
+
+    else:
+        return {"Response from API": "Invalid Request"}
---
+++ validation.py
@@ -0,0 +1,6 @@
+def validate_icto(icto):
+    if icto == "31400":
+        print("ICTO is validated")
+        return True
+    else:
+        return False
